42c38914d183435eb1a892e677acd177a4f0e6e0
Michael Saraceno <msaraceno@payomatic.com>
2018-07-07 12:56:27 -0400

    intial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-40142d25"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "terraform-example"
+  }
+}
+
