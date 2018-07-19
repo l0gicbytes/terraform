3de8024eb53257b8a867dcea51114620620bc3ed
l0gicbytes <mike@mikeandcathy.net>
2018-07-19 17:12:41 -0400

    adding encryption, aws SSE-S3 at the bucket level

--- terraform_state/main.tf
+++ terraform_state/main.tf
@@ -15,13 +15,13 @@ terraform {
 
 resource "aws_s3_bucket" "terraform_state" {
     bucket = "mike-terraform-state.pom.com"
-/*    server_side_encryption_configuration {
+    server_side_encryption_configuration {
       rule {
         apply_server_side_encryption_by_default {
           sse_algorithm = "AES256"
         }
       }
-    }*/
+    }
     versioning {
       enabled = true
     }
