93fcebf638923532b51488804374267389517a9d
l0gicbytes <mike@mikeandcathy.net>
2018-07-23 17:20:00 -0400

    adding more outputs

--- projects/web_server/database/output.tf
+++ projects/web_server/database/output.tf
@@ -1,3 +1,15 @@
 output "Secret" {
   value = "${data.aws_secretsmanager_secret_version.server_pw.secret_string}"
 }
+
+output "address" {
+  value = "${aws_db_instance.example.address}"
+}
+
+output "port" {
+  value = "${aws_db_instance.example.port}"
+}
+
+output "endpoint" {
+  value = "${aws_db_instance.example.endpoint}"
+}
